06006373b9933d7fb7a954efd29479a9af3afe8e
Thu Pham <thu.anh.pham24@gmail.com>
2025-02-09 20:25:50 -0500

    Create color_extract.py

---
+++ Day18_GridPainting/color_extract.py
@@ -0,0 +1,13 @@
+import colorgram as cg
+
+colors = cg.extract("image.jpeg", 2 ** 32)
+rgb_colors = []
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
